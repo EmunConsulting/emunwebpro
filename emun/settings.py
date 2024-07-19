eed046f6f145b04898c1e2b0832ed56cabb832f5
emunconslting <emunconsultingltd@gmail.com>
2024-07-19 10:56:08 +0300

    changed Debug mode and added ip of the droplet

--- emun/settings.py
+++ emun/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e7z6af&n@d4dq2gu3@7wzb&mg&3iy^sdd!k2oqwbx)e5(fg2zx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['161.35.192.27']
 
 
 # Application definition
